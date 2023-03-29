99fef9c472150a4a5c1235fff6c1b03f249d3f5e
kristenmazza <kbmazza@gmail.com>
2023-03-28 19:20:49 -0700

    Replace main image and update copy content

--- src/page-load.js
+++ src/page-load.js
@@ -1,5 +1,6 @@
 import './style.css';
-import Overcooked from './overcooked.jpeg';
+import Overcooked from './overcookedheader.png';
+import OvercookedLogo from './overcookedlogo.png';
 
 function componentContent() {
   const content = document.createElement('div');
@@ -16,11 +17,11 @@ function componentHeader() {
 }
 
 function componentName() {
-  const name = document.createElement('div');
-  name.classList.add('restaurant-name');
-  name.textContent = 'Restaurant Name';
+  const logo = new Image();
+  logo.src = OvercookedLogo;
+  logo.classList.add('logo');
 
-  return name;
+  return logo;
 }
 
 function componentTabContainer() {
@@ -37,17 +38,23 @@ function componentTabContentContainer() {
   return tabContentContainer;
 }
 
-
 function componentHeadline() {
   const headline = document.createElement('h1');
-  headline.textContent = 'My Restaurant';
+  headline.textContent = 'Welcome to Overcooked Restaurant!';
 
   return headline;
+}
+function componentCopyContainer() {
+  const copyContainer = document.createElement('div');
+  copyContainer.classList.add('copy-container');
+
+  return copyContainer;
 }
 
 function componentCopy() {
   const copy = document.createElement('div');
-  copy.textContent = 'This is my copy.';
+  copy.classList.add('copy', 'pre');
+  copy.textContent = `Welcome to Overcooked Restaurant, where culinary chaos reigns supreme! If you're looking for an exciting dining experience that's sure to satisfy your appetite for adventure, look no further than Overcooked. \n\nWith a menu that's constantly changing and evolving, there's always something new to try at Overcooked. Whether you're in the mood for classic comfort food or innovative fusion cuisine, our talented chefs are always pushing the boundaries of what's possible. \n\nSo gather your friends and family, and join us for a meal that's sure to be an unforgettable experience. At Overcooked Restaurant, the heat is always on and the excitement is never-ending!`;
 
   return copy;
 }
@@ -60,8 +67,10 @@ function componentFooter() {
 }
 
 function componentImage() {
-  // const mainImage = document.createElement('img');
-  // mainImage.src = Overcooked;
+  // const mainImage = document.createElement('div');
+  // mainImage.style.backgroundImage = `url(${Overcooked})`;
+  // mainImage.classList.add('main-image');
+
   const mainImage = new Image();
   mainImage.src = Overcooked;
   mainImage.classList.add('main-image');
@@ -76,6 +85,7 @@ export const name = componentName();
 export function init() {
   const content = componentContent();
   const header = componentHeader();
+  const copyContainer = componentCopyContainer();
 
   document.body.appendChild(content);
   content.appendChild(header);
@@ -83,11 +93,9 @@ export function init() {
   header.appendChild(tabContainer);
   content.appendChild(tabContentContainer);
   tabContentContainer.appendChild(componentImage());
-  tabContentContainer.appendChild(componentHeadline());
-  tabContentContainer.appendChild(componentCopy());
+  tabContentContainer.appendChild(copyContainer);
+  copyContainer.appendChild(componentHeadline());
+  copyContainer.appendChild(componentCopy());
   content.appendChild(componentFooter());
 }
 
-
-// create container for tabs, content, footer; export tab container (div with buttons) and tab content container (e.g., container with home, menu, etc.)
-// only things you manipulate need to be exported
