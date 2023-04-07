8631e860c79f6cc481181303b895af2da8394b31
kristenmazza <kbmazza@gmail.com>
2023-04-06 17:08:32 -0700

    Add style to tab buttons

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ import { tabContainer, init } from './page-load';
 
 init();
 
-// const tabContent = document.createElement('div');
-// tabContentContainer.appendChild(tabContent);
-// tabContent.classList.add('tab-content');
-
 const home = document.createElement('button');
 home.textContent = 'HOME';
 tabContainer.appendChild(home);
+home.classList.add('button');
 
 const menu = document.createElement('button');
 menu.textContent = 'MENU';
 tabContainer.appendChild(menu);
+menu.classList.add('button');
 
 const contact = document.createElement('button');
 contact.textContent = 'CONTACT';
 tabContainer.appendChild(contact);
+contact.classList.add('button');
+
 
 
 
