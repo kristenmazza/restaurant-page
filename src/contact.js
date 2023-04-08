8375f716da2240d982b8de5177362b13c05bbcc7
kristenmazza <kbmazza@gmail.com>
2023-04-08 11:59:29 -0700

    Add content to Contact tab

--- src/contact.js
+++ src/contact.js
@@ -30,11 +30,106 @@ function componentContactHeader() {
     return contactHeader;
 }
 
+function componentPhoneNumber() {
+    const phoneNumber = document.createElement('p');
+    phoneNumber.classList.add('contact-details');
+    phoneNumber.textContent = 'PHONE: 555-5555-FOOD';
+
+    return phoneNumber;
+}
+
+function componentEmail() {
+    const email = document.createElement('p');
+    email.classList.add('contact-details');
+    email.textContent = 'EMAIL: info@restaurant.com';
+
+    return email;
+}
+
+function componentContactForm() {
+    const contactForm = document.createElement('form');
+    contactForm.setAttribute('method', 'post');
+    contactForm.setAttribute('action', '');
+    contactForm.classList.add('contact-form');
+
+    return contactForm;
+}
+
+function componentFormInputName() {
+    const formInputName = document.createElement('input');
+    formInputName.setAttribute('type', 'text');
+    formInputName.setAttribute('id', 'name');
+    formInputName.setAttribute('name', 'name');
+
+    return formInputName;
+}
+
+function componentFormLabelName() {
+    const formLabelName = document.createElement('label');
+    formLabelName.setAttribute('for', 'name');
+    formLabelName.innerHTML = 'FULL NAME';
+
+    return formLabelName;
+}
+
+function componentFormInputEmail() {
+    const formInputEmail = document.createElement('input');
+    formInputEmail.setAttribute('type', 'email');
+    formInputEmail.setAttribute('id', 'email');
+    formInputEmail.setAttribute('name', 'email');
+
+    return formInputEmail;
+}
+
+function componentFormLabelEmail() {
+    const formLabelEmail = document.createElement('label');
+    formLabelEmail.setAttribute('for', 'email');
+    formLabelEmail.innerHTML = 'EMAIL ADDRESS';
+
+    return formLabelEmail;
+}
+
+function componentFormInputMessage() {
+    const formInputMessage = document.createElement('textarea');
+    formInputMessage.setAttribute('id', 'message');
+    formInputMessage.setAttribute('name', 'message');
+
+    return formInputMessage;
+}
+
+function componentFormLabelMessage() {
+    const formLabelMessage = document.createElement('label');
+    formLabelMessage.setAttribute('for', 'message');
+    formLabelMessage.innerHTML = 'MESSAGE';
+
+    return formLabelMessage;
+}
+
+function componentSubmitButton() {
+    const submitButton = document.createElement('button');
+    submitButton.setAttribute('type', 'submit');
+    submitButton.textContent = 'SUBMIT';
+
+    return submitButton;
+}
+
 export function componentContactContent() {
     const contactContainer = componentContactContainer();
     const contactFormContainer = componentContactFormContainer();
+    const contactForm = componentContactForm();
+
     contactContainer.appendChild(contactFormContainer);
     contactFormContainer.appendChild(componentContactHeader());
+    contactFormContainer.appendChild(componentPhoneNumber());
+    contactFormContainer.appendChild(componentEmail());
+    contactFormContainer.appendChild(contactForm);
+    contactForm.appendChild(componentFormLabelName())
+    contactForm.appendChild(componentFormInputName());
+    contactForm.appendChild(componentFormLabelEmail())
+    contactForm.appendChild(componentFormInputEmail());
+    contactForm.appendChild(componentFormLabelMessage())
+    contactForm.appendChild(componentFormInputMessage());
+    contactForm.appendChild(componentSubmitButton());
 
     return [contactContainer];
 }
